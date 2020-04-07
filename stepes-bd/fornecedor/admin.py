a62e42ce6bec2761521aa7ae98c4af0b670635c0
jhassam <juan.hassam@compsis.com.br>
2020-04-07 19:23:06 -0300

    edit

--- stepes-bd/fornecedor/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from fornecedor.models import Fornecedor
-
-# Pedindo pro django admin conseguir "cuidar" do model de Cliente
-# No django admin se consegue listrar, criar, excluir e editar um Cliente
-admin.site.register(Fornecedor)
